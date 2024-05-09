6a0d057f6333d011c66ea6bd1f96f82c67b94b1d
Ricardo <20230332@ricaldone.edu.sv>
2024-05-09 13:22:09 -0600

    Se continuó el crud*3

--- app/src/main/java/RecyclerViewHelper/Adaptador.kt
+++ app/src/main/java/RecyclerViewHelper/Adaptador.kt
@@ -3,6 +3,10 @@ package RecyclerViewHelper
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import modelo.ClaseConexion
 import modelo.dataclassProductos
 import ricardo.galvez.crudri.R
 
@@ -10,6 +14,31 @@ import ricardo.galvez.crudri.R
 
         fun actualizarLista(nuevaLista: List<dataclassProductos>){
             Datos = nuevaLista
+            notifyDataSetChanged()
+        }
+
+        fun eliminarRegistro(nombreProducto: String, posicion: Int){
+
+            //1- Crear un objeto de la clase conexion
+            val objConexion = ClaseConexion().cadenaConexion()
+
+            //Quitar el elemento de la lista
+            val listaDatos = Datos.toMutableList()
+            listaDatos.removeAt(posicion)
+
+            //Quitar de la base de datos
+            GlobalScope.launch(Dispatchers.IO){
+                val delProducto = objConexion?.prepareStatement("delete tbProductos where nombreProducto = ?")!!
+                delProducto.setString(1, nombreProducto)
+                delProducto.executeUpdate()
+
+                val commit = objConexion.prepareStatement("commit")!!
+                commit.executeUpdate()
+            }
+
+            //le decimos al adaptador que se eliminaron los datos
+            Datos = listaDatos.toList()
+            notifyItemRemoved(posicion)
             notifyDataSetChanged()
         }
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -21,5 +50,10 @@ import ricardo.galvez.crudri.R
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
             val producto = Datos[position]
             holder.textView.text = producto.nombreProducto
+
+            val item = Datos[position]
+            holder.imgBorrar.setOnClickListener(){
+                eliminarRegistro(item.nombreProducto, position)
+            }
         }
     }
